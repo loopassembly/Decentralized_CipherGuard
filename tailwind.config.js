2ce5b3c5b9e57a03ff0845c309ca0d54af1d02d5
ashutosh <ashutoshanand2560@gmail.com>
2022-06-23 13:02:07 +0530

    update 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['app/core/templates/index.html','app/core/templates/login.html','app/core/templates/forget.html'],
+  content: [
+    "./app/core/templates/test.html",
+    "./app/core/templates/forget.html",
+    "./app/core/templates/index.html",
+    "./app/core/templates/login.html",
+    "./app/core/templates/password.html",
+    "./app/core/templates/signup.html",
+  
+
+
+],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/typography"),require("daisyui"),
-  ],
-   // daisyUI config (optional)
-   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
+  plugins: [require("daisyui")],
 }
