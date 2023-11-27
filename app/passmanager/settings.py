fb7588d8b8d4fee21740b3ff232f3a3e7c76bbd6
Ashutosh Anand <ashutoshanand2560@gmail.com>
2023-11-27 23:48:34 +0530

    randomjs

--- app/passmanager/settings.py
+++ app/passmanager/settings.py
@@ -179,6 +179,9 @@ LOGIN_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_HOST_USER = 'loopassembly@gmail.com'  
-EMAIL_HOST_PASSWORD = 'cpezwrwbwiaxypub'  
+EMAIL_HOST_USER = 'loopassembly@gmail.com'
+EMAIL_HOST_PASSWORD ='dbtmwotfkbjjkvha' 
+
 EMAIL_PORT = 587  
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://10.3.82.194:8080/','http://10.3.82.194:2560']
+# django_heroku.settings(locals())
