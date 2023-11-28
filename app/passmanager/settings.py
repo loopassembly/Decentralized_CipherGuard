011dc8b7184fbfeb2399c4ab4cd50a2f52ac13d3
Ashutosh Anand <ashutoshanand2560@gmail.com>
2023-11-29 02:44:06 +0530

    Update settings.py

--- app/passmanager/settings.py
+++ app/passmanager/settings.py
@@ -180,7 +180,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_HOST_USER = 'loopassembly@gmail.com'
-EMAIL_HOST_PASSWORD ='dbtmwotfkbjjkvha' 
+EMAIL_HOST_PASSWORD ='<your password>' 
 
 EMAIL_PORT = 587  
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://10.3.82.194:8080/','http://10.3.82.194:2560']
